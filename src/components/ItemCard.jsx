98a5d1e6604406544f57946b66c844c788906e32
Taynah-M-Alves <taynahdemichelialves@gmail.com>
2025-07-16 14:24:35 -0400

    atualizando estilização do card-grid

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -2,20 +2,20 @@
 
 export default function ItemCard({ nome, descricao, condicao, foto }) {
   return (
-    <div className="bg-white rounded-2xl min-w-80 shadow-md p-4 border border-gray-200 hover:shadow-laranja ">
+    <div className="bg-gray-800 rounded-2xl min-w-60 shadow-md p-4 border border-gray-700 hover:border-amber-600 ">
       <img
         src={foto}
         alt={nome}
         className="h-90 object-cover rounded-xl mb-4"
       />
-      <h2 className="text-gray-900 text-lg font-bold">{nome}</h2>
-      <p className="text-gray-600 text-sm mb-1">{descricao}</p>
+      <h2 className="text-white text-lg font-bold">{nome}</h2>
+      <p className="text-gray-300 text-sm mb-1">{descricao}</p>
       <span
         className={`inline-block text-xs px-2 py-1 rounded-full ${
           condicao === "Novo"
             ? "bg-green-100 text-green-700"
             : condicao === "Seminovo"
-            ? "bg-yellow-100 text-yellow-700"
+            ? "bg-yellow-100 text-yellow-300"
             : "bg-gray-200 text-gray-700"
         }`}
       >
--- src/pages/Items-grid.jsx
+++ src/pages/Items-grid.jsx
@@ -2,18 +2,20 @@ import ItemCard from '../components/itemcard';
 
 export function ItemsGrid({ items }) {
   return (
-    <div className="p-8 flex flex-col ">
-      <h1 className=" text-7xl font-bold mb-6 self-center">Lista de Itens</h1>
-      <h2 className=" text-4xl font-serif mb-6 self-center">Conheça os itens disponíveis para troca.</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-11/12 justify-items-center">
-        {items.map((item) => (
-          <ItemCard
-            nome={item.nome}
-            descricao={item.descricao}
-            condicao={item.condicao}
-            foto={item.foto}
-          />
-        ))}
+    <div className="p-8 flex flex-col bg-gray-900">
+      <div className=''>
+        <h1 className=" text-7xl font-bold mb-6 self-center">Lista de Itens</h1>
+        <h2 className=" text-4xl font-serif mb-6 self-center">Conheça os itens disponíveis para troca.</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-10/12 justify-items-center mx-auto ">
+          {items.map((item) => (
+            <ItemCard
+              nome={item.nome}
+              descricao={item.descricao}
+              condicao={item.condicao}
+              foto={item.foto}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
